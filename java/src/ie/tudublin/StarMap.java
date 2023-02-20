6e75e5eb90074be96b13d5a5708490f8bfb5e278
Darren Grants <darrengrants06@gmail.com>
2023-02-20 12:12:50 +0000

    i have no clue, bunch of merges

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,6 +1,10 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 import processing.data.TableRow;
 import processing.data.Table;
@@ -9,6 +13,8 @@ import java.util.ArrayList;
 
 public class StarMap extends PApplet
 {
+	ArrayList<Star> stars = new ArrayList<Star>();
+
 	public void settings()
 	{
 		size(800, 800);
@@ -16,7 +22,7 @@ public class StarMap extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
 		
 		smooth();
@@ -24,14 +30,37 @@ public class StarMap extends PApplet
 		printStars();
 	}
 
+	void printStars()
+	{
+		for(int i = 0 ; i < stars.size() ; i ++)
+		{
+			println(stars.get(i));
+		}
+	}
+
+	void displayStars()
+	{
+		for(int i = 0 ; i < stars.size() ; i ++)
+		{
+			stars.get(i).render(this);
+		}
+	}
+
+	public void loadStars()
+	{
+		Table table = loadTable("HabHYG15ly.csv", "header");
+ 		for(TableRow r:table.rows())
+ 		{
+ 			Star s = new Star(r);
+ 			stars.add(s);
+ 		}
+	}
 
 	public void drawGrid()
 	{
-		stroke(255);
-		float border = 50.0f;
+		stroke(0, 255, 255);
+		float border = width * 0.1f;
 
-		int count = 10;
-		float gap = (width - (border * 2.0f)) / (float) count;
 		for(int i = -5 ; i <= 5 ; i ++)
 		{
 
@@ -44,6 +73,9 @@ public class StarMap extends PApplet
 			text(i, x, border - 20);
 			text(i, border - 20, x);
 		}
+
+		//float f = map(5, 0, 10, 100, 200);
+		//float f1 = map1(5, 0, 10, 100, 200);
 		
 	}
 	
