4b026f84cd3f2aec12b0ea250c10680045f19140
Darren Grants <darrengrants06@gmail.com>
2023-02-27 12:56:53 +0000

    audio 2 class work

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -26,6 +26,18 @@ public class Main
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new StarMap());
     }
+
+	public static void audio1()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
+
+	public static void audio2()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new audio2());
+    }
 	
 	public static void main(String[] args)
 	{
@@ -35,7 +47,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		starMap();
+		audio2();
 	}
 	
 }
--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -11,150 +11,106 @@ import processing.data.Table;
 
 import java.util.ArrayList;
 
-public class StarMap extends PApplet
-{
+import processing.data.*;
+
+public class StarMap extends PApplet {
+
 	ArrayList<Star> stars = new ArrayList<Star>();
 
-	public void settings()
-	{
-		size(800, 800);
-		loadStars();
-	}
+	public float border;
 
-	public void setup() {
-		colorMode(RGB);
-		background(0);
-		
-		smooth();
-<<<<<<< HEAD
-=======
-
->>>>>>> 043d21d1b70f553886277dd4997a9e0376844936
-		loadStars();
-		printStars();
-	}
-
-	void printStars()
-	{
-		for(int i = 0 ; i < stars.size() ; i ++)
-		{
-			println(stars.get(i));
-		}
-	}
-
-	void displayStars()
-	{
-		for(int i = 0 ; i < stars.size() ; i ++)
-		{
-			stars.get(i).render(this);
-		}
-	}
-
-	public void loadStars()
-	{
-		Table table = loadTable("HabHYG15ly.csv", "header");
- 		for(TableRow r:table.rows())
- 		{
- 			Star s = new Star(r);
- 			stars.add(s);
- 		}
-	}
-
-	public void drawGrid()
-	{
-		stroke(0, 255, 255);
-		float border = width * 0.1f;
-
-		for(int i = -5 ; i <= 5 ; i ++)
-		{
-<<<<<<< HEAD
-
-			float x = border + (gap * (i + 5));
-			stroke(255,100,100);
-			line(x, border, x, height - border);
-			line(border, x, width - border, x);
-
-			fill(265,70,80);
-			text(i, x, border - 20);
-			text(i, border - 20, x);
-=======
+	void drawGrid() {
+		stroke(255, 0, 255);
+		textAlign(CENTER, CENTER);
+		textSize(20);
+		for (int i = -5; i <= 5; i++) {
 			float x = map(i, -5, 5, border, width - border);
 			line(x, border, x, height - border);
 			line(border, x, width - border, x);
-
-			textAlign(CENTER, CENTER);
+			fill(255);
 			text(i, x, border * 0.5f);
 			text(i, border * 0.5f, x);
->>>>>>> 043d21d1b70f553886277dd4997a9e0376844936
+		}
+	}
+
+	public class StarMap extends PApplet
+	{
+		ArrayList<Star> stars = new ArrayList<Star>();
+
+		public void settings() {
+			size(800, 800);
+			loadStars();
 		}
 
+		public void setup() {
+			colorMode(RGB);
+			background(0);
+
+			smooth();
+			loadStars();
+			printStars();
+		}
+
+		void printStars() {
+			for (Star s : stars) {
+				System.out.println(s);
+			}
+		}
+
+		void loadStars() {
+			Table table = loadTable("HabHYG15ly.csv", "header");
+			for (TableRow r : table.rows()) {
+				Star s = new Star(r);
+				stars.add(s);
+			}
+		}
+
+		public void settings() {
+			size(800, 800);
+		}
+
+		Star first = null;
+		Star second = null;
+
+			public void mouseClicked()
+			{
+				for(Star s:stars)
+				{
+					float x = map(s.getxG(), -5, 5, border, width - border);
+					float y = map(s.getyG(), -5, 5, border, height - border);
+
+					for(int i = -5 ; i <= 5 ; i ++)
+					{
+						float x = map(i, -5, 5, border, width - border);
+						line(x, border, x, height - border);
+						line(border, x, width - border, x);
+
+						textAlign(CENTER, CENTER);
+						text(i, x, border * 0.5f);
+						text(i, border * 0.5f, x);
+					}	
+
+				}
+			}
 		//float f = map(5, 0, 10, 100, 200);
 		//float f1 = map1(5, 0, 10, 100, 200);
 		
-	}
-<<<<<<< HEAD
 	
-	ArrayList<Star> stars = new ArrayList<Star>();	
-	
-	void loadStars()
- 	{
- 		Table table = loadTable("HabHYG15ly.csv", "header");
- 		for(TableRow r:table.rows())
- 		{
- 			Star s = new Star(r);
- 			stars.add(s);
- 		}
- 	}
 
-	void printStars()
-	{
-		for(Star s:stars)
+		float map1(float a, float b, float c, float d, float e)
 		{
-			System.out.println(s);
+			float r1 = c - b;
+			float r2 = e - d;
+
+			float howFar = a - b;
+
+			return d + ((howFar / r1) * r2);
 		}
-	}
 
-	public void draw()
-	{	
-	 	strokeWeight(2);		
- 
-		drawGrid();
-		drawStar();
-=======
+		public void draw() {
+			strokeWeight(1);
 
-	float map1(float a, float b, float c, float d, float e)
-	{
-		float r1 = c -b;
-		float r2 = e - d;
-
-		float howFar = a - b;
-
-		return d + ((howFar / r1) * r2);
-	}
-		
-	public void draw()
-	{	
-		strokeWeight(1);		
-
-		drawGrid();
-		displayStars();
->>>>>>> 043d21d1b70f553886277dd4997a9e0376844936
-	}
-
-	public void drawStar()
-	{
-		for(Star s:stars)
-		{
-			float x = map(s.xG,-5,5,50, width - 50);
-			float y = map(s.yG,-5,5,50, width -50);
-			fill(120,100,100);
-			stroke (255,255,255);
-			
-			noFill();
-			circle(x,y,s.absMag);
-
-			text(s.displayName, x, y);
+			drawGrid();
+			displayStars();
 		}
-	}
- 
 }
