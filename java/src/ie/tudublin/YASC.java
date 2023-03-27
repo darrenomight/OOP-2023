805daa2fef23c03304b0e539b49afa0ec2d5a9be
Darren Grants <darrengrants06@gmail.com>
2023-03-27 14:23:23 +0100

    uploading changes

--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -5,23 +5,30 @@ import processing.core.PApplet;
 public class YASC extends PApplet
 {
 
-	Ship ship;
+    Ship ship;
+    Ship ship1;
 
-	public void settings()
-	{
-		size(500, 500);
-		
-	}
+    public void settings()
+    {
+        size(500, 500);
+    }
 
-	public void setup() {
-		ship = new Ship(width / 2, height / 2, 50, 70, this);
-		colorMode(HSB);
-	}
+    public void setup()
+    {
+        colorMode(HSB);
+        ship = new Ship(width / 2, height / 2, 50, 70, this);
+        ship1 = new Ship(100, 50, 80, 6, this);
+        
+    }
+    
+    
+    public void draw()
+    {    
+        background(0);
+        ship.render();
+        ship.move();
 
-	
-	
-	public void draw()
-	{	
-		ship.render();
-	}
+        ship1.render();
+        ship1.move();
+    }
 }
